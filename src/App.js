ba0ac902d3cee8f23de1c3ddf5eb7ea2adffcbab
smiethereens23nas <nicholas23smith@gmail.com>
2022-02-28 12:14:03 -0500

    Set up my routers and have it rendering on page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-hi
+import React from "react";
+// import ReactDOM from "react-dom";
+import "./index.css";
+// import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import Currencies from "./pages/Currencies";
+import Main from "./pages/Main";
+import Price from "./pages/Price";
+import Nav from "./components/Nav";
+
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import your route components too
 function App() {
-  return (
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Price />}></Route>
+          <Route path="/currencies" element={<Currencies />}></Route>
+          <Route path="/price" element={<Price />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
-
 export default App;
